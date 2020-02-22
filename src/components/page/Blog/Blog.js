b507eb51040c02ecd2ad0d8c39bbf9f3ad6a1cf5
jora <jora@email.ru>
2020-02-22 18:34:23 +0300

    add mobx

--- src/components/page/Blog/Blog.js
+++ src/components/page/Blog/Blog.js
@@ -1,43 +1,14 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { Media } from 'react-bootstrap';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 
+@observer
 class Blog extends Component {
-  constructor(props) {
-    super(props);
+  @observable blogs = [];
 
-    this.state = { users: [] };
-  }
-
-  componentDidMount() {
-    axios.get('https://reqres.in/api/users?page=1')
-      .then(response => this.setState({ users: response.data.data }));
-  }
-
-  renderUsers() {
-    const { users } = this.state;
-
-    return users.map(user => (
-      <Media key={user.id}>
-        <img
-          width={256}
-          height={256}
-          className='mr-3'
-          src={user.avatar}
-          alt='Generic placeholder'
-        />
-        <Media.Body>
-          <h5>Media Heading</h5>
-          <p>
-            {user.first_name} {user.last_name}
-          </p>
-        </Media.Body>
-      </Media>
-    ));
-  }
 
   render() {
-    return <div>{this.renderUsers()}</div>;
+    return <div>Blog content</div>;
   }
 }
 
